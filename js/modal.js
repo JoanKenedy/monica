d22fd10b83fa53f44e86c1eac66668c6b7218183
leonelvalentinn <leonelvalentinn@gmail.com>
2023-11-10 06:56:59 -0600

    promos buen fin

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById("modalPromo");
+
+window.addEventListener("load", function (event) {
+  const time = setTimeout(myModal, 2000);
+});
+
+function myModal() {
+  const close = document.getElementById("close");
+  modal.classList.add("modal-show");
+
+  close.addEventListener("click", () => {
+    modal.classList.remove("modal-show");
+  });
+}
