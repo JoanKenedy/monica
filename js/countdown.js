bae0d41257c27e64fcb7a88fdcb39e6dbc5f34a8
leonelvalentinn <leonelvalentinn@gmail.com>
2024-05-30 20:07:12 -0600

    Changes

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ const SECOND = 1000
 const MINUTE = SECOND * 60
 const HOUR = MINUTE * 60
 const DAYS = HOUR * 24
-const END_DATE = 1718470335000
+const END_DATE = 1718355600000
 const date = new Date(END_DATE).getTime()
 
 const $timer = [...document.querySelectorAll('#timer')]
@@ -14,7 +14,7 @@ const intervalCountdown = () => {
   const hourText = Math.floor((diff % DAYS) / HOUR).toString().padStart(2, '0')
   const minuteText = Math.floor((diff % HOUR) / MINUTE).toString().padStart(2, '0')
   const secondText = Math.floor((diff % MINUTE) / SECOND).toString().padStart(2, '0')
-  
+
   $timer[0].innerHTML = dayText
   $timer[1].innerHTML = hourText
   $timer[2].innerHTML = minuteText
@@ -23,6 +23,6 @@ const intervalCountdown = () => {
 
 intervalCountdown()
 
-  setInterval(() => {
-    intervalCountdown()
-  }, SECOND)
+setInterval(() => {
+  intervalCountdown()
+}, SECOND)
